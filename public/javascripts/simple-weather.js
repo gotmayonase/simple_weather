df31824c8b538c26520821b8a1092b735a64ae51
Mike Mayo <gotmayonase@gmail.com>
2013-10-30 22:08:52 -0500

    wiring up to the api on index.html

--- public/javascripts/simple-weather.js
+++ public/javascripts/simple-weather.js
@@ -1,91 +1,138 @@
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position){
-      gotLocation(position.coords.latitude,position.coords.longitude)
-      geocodePosition(position);
-    }, function(error){
-      gotLocation(32,100)
-      console.log(error);
-    });
-  }
-  else {
-    console.log('not locating')
-  }
-}
+(function(){
+  var index, scroll, times, count, delta, middle, increment, units;
+	refreshDefaults();
 
-function gotLocation(latitude, longitude) {
-  $.getJSON('/forecast/' + latitude + ',' + longitude, function(data) {
-    var units = data.flags.units == 'us' ? 'F' : 'C';
+  function precision( number , precision ){
+    var prec = Math.pow( 10 , precision );
+    return Math.round( number * prec ) / prec;
+  }        
 
-    $('.temp').html([parseInt(data.currently.temperature, 10),'&deg;',units].join(''))
-    $('.condition').html(data.currently.summary)
+  $(document).on('mousewheel', function(e){
 
-    $('#weatherSummary ul').html('');
-    $('#weatherSummary ul').append('<li>' + data.currently.summary + '</li>')
-    $('#weatherSummary ul').append('<li>' + Math.round(data.currently.temperature) + '&deg;' + units + '</li>')
-    $('#weatherSummary ul').append('<li>' + Math.round(data.currently.precipProbability*100) + '% chance of precipitation</li>')
-    $('#weatherSummary ul').append('<li>High: ' + Math.round(data.daily.data[0].temperatureMax) + '&deg;' + units + '</li>')
-    $('#weatherSummary ul').append('<li>Low: ' + Math.round(data.daily.data[0].temperatureMin) + '&deg;' + units + '</li>')
-    $('#hourlyData').html('');
-    $.each(data.hourly.data, function(index, tempObject) {
-      var date = new Date(tempObject.time * 1000);
-      var hours = date.getHours();
-      var amPM = 'am';
-      if(hours > 12){
-        hours = hours - 12;
-        amPM = 'pm'
-      } else if(hours == 0) {
-        hours = 12
-      } else if(hours == 12){
-        hours = 12
-        amPM = 'pm'
-      }
-      $('#hourlyData').append('<section> \
-        <h1>' + hours + ':00' + amPM +'</h1> \
-        <ul> \
-          <li>' + tempObject.summary + '</li> \
-          <li>' + Math.round(tempObject.temperature) + '&deg;' + units + '</li> \
-          <li>' + Math.round(tempObject.precipProbability*100) + '% chance of precipitation</li> \
-        </ul> \
-      </section>')
-    })
-  })
-}
+    index += ((e.originalEvent.wheelDelta > 0 ? 1 : -1) * increment);
 
-function geocodePosition(position) {
-  geocoder = new google.maps.Geocoder();
-  var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  geocoder.geocode({'latLng': latlng}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      if (results[1]) {
-        $('.location').html(results[1].formatted_address)
-      }
-    } else {
-      alert("Geocoder failed due to: " + status);
+    index = precision(index,2)
+
+    if (index <= 0) {
+      index = 0
     }
-  });
-}
+    else if (index >= count) {
+      index = count - 1
+    }
 
-$(function(){
-  getLocation();
-  $('form').submit(function() {
-    var address = $('#searchField').val();
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        position = results[0].geometry.location;
-        gotLocation(position.lb, position.mb);
-        $('h2').html(results[0].formatted_address);
-      } else {
-        console.log("Geocode was not successful for the following reason: " + status);
-        console.log(results);
-      }
-    });
-    return false;
-  });
-  
-  $('#currentLocation').click(function(e){
-    getLocation();
-    $('#searchField').val('');
+    if (index % 1 == 0) {
+      var $li = times.eq(index);
+      times.removeClass('active')
+      $li.addClass('active')
+			setWeather($li.data('temp'), $li.data('summary'))
+      scroll.css({
+        left: (index == count ? index : ($li.position().left * -1)) + middle - 25
+      })            
+    }
+
     e.preventDefault();
-  });
-});
+    return false;
+  })
+
+	function getLocation() {
+	  if (navigator.geolocation) {
+	    navigator.geolocation.getCurrentPosition(function(position){
+	      gotLocation(position.coords.latitude,position.coords.longitude)
+	      geocodePosition(position);
+	    }, function(error){
+	      gotLocation(32,100)
+	      console.log(error);
+	    });
+	  }
+	  else {
+	    console.log('not locating')
+	  }
+	}
+
+	function gotLocation(latitude, longitude) {
+	  $.getJSON('/forecast/' + latitude + ',' + longitude, function(data) {
+	    units = data.flags.units == 'us' ? 'F' : 'C';
+			
+			setWeather(data.currently.temperature, data.currently.summary, '');
+			$('.times').html('');
+			var new_items = [];
+	    $.each(data.hourly.data, function(index, tempObject) {
+	      var date = new Date(tempObject.time * 1000);
+	      var hours = date.getHours();
+	      var amPM = 'am';
+	      if(hours > 12){
+	        hours = hours - 12;
+	        amPM = 'pm'
+	      } else if(hours == 0) {
+	        hours = 12
+	      } else if(hours == 12){
+	        hours = 12
+	        amPM = 'pm'
+	      }
+				var $li = $('<li>' + hours + ':00' + amPM +'</li>');
+				$li.data('temp', tempObject.temperature);
+				$li.data('precip', tempObject.precipProbability);
+				$li.data('summary', tempObject.summary);
+				$li.data('icon', tempObject.icon);
+				new_items.push($li);
+	    });
+			$('.times').append(new_items);
+			refreshDefaults();
+	  })
+	}
+	
+	function refreshDefaults() {
+		index     = 0;
+	  scroll    = $('.time-scroll');
+	  times     = scroll.find('li');
+	  count     = times.length;
+	  delta     = 0;
+	  middle    = scroll.innerWidth()/2;
+	  increment = .1;
+		scroll.css({left:middle})
+	}
+	
+	function setWeather(temp, condition, percipProb) {
+		$('.temperature').html([parseInt(temp, 10),'&deg;',units].join(''))
+    $('.condition').html(condition)
+	}
+
+	function geocodePosition(position) {
+	  geocoder = new google.maps.Geocoder();
+	  var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+	  geocoder.geocode({'latLng': latlng}, function(results, status) {
+	    if (status == google.maps.GeocoderStatus.OK) {
+	      if (results[1]) {
+	        $('.location').html(results[1].formatted_address)
+	      }
+	    } else {
+	      alert("Geocoder failed due to: " + status);
+	    }
+	  });
+	}
+	
+	$(function(){
+	  getLocation();
+	  $('form').submit(function() {
+	    var address = $('#searchField').val();
+	    geocoder.geocode( { 'address': address}, function(results, status) {
+	      if (status == google.maps.GeocoderStatus.OK) {
+	        position = results[0].geometry.location;
+	        gotLocation(position.lb, position.mb);
+	        $('h2').html(results[0].formatted_address);
+	      } else {
+	        console.log("Geocode was not successful for the following reason: " + status);
+	        console.log(results);
+	      }
+	    });
+	    return false;
+	  });
+
+	  $('#currentLocation').click(function(e){
+	    getLocation();
+	    $('#searchField').val('');
+	    e.preventDefault();
+	  });
+	});
+	
+})();
